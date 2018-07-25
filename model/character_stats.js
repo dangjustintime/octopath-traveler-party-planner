b28911fe84d5dcc8a1aa8cedb5a0a8341447448e
Justin Dang <dangjustintime@gmail.com>
2018-07-25 12:16:02 -0400

    added character_stats.js, added Olberic's lvl 1 stats

---
+++ model/character_stats.js
@@ -0,0 +1,34 @@
+// stats of all the characters at level 1
+// stats found from gamefaqs forum post:
+//   https://gamefaqs.gamespot.com/boards/204212-octopath-traveler/76722867
+
+// Ophilia
+
+// Cyrus
+
+// Tressa
+
+// Olberic
+const Olberic = {
+    name: "Olberic",
+    class: "warrior",
+    level: 1,
+    max_hp: 325,
+    max_sp: 40,
+    phys_atk: 96,
+    elem_atk: 72,
+    phys_def: 88,
+    elem_def: 64,
+    accuracy: 88,
+    speed: 80,
+    critical: 80,
+    evasion: 64
+}
+
+// Primrose
+
+// Alfyn
+
+// Therion
+
+// H'aanit
