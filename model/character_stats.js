4d69b5dd94cb1eddf430f96520934f65b3d80f4d
Justin Dang <dangjustintime@gmail.com>
2018-07-25 12:28:02 -0400

    added tressa, cyrus, and ophilia lvl 1 stats to character_stats.js

--- model/character_stats.js
+++ model/character_stats.js
@@ -3,10 +3,55 @@
 //   https://gamefaqs.gamespot.com/boards/204212-octopath-traveler/76722867
 
 // Ophilia
+const Ophilia = {
+    name: "Ophilia",
+    class: "Cleric",
+    level: 1,
+    max_hp: 225,
+    max_sp: 65,
+    phys_atk: 80,
+    elem_atk: 96,
+    phys_def: 80,
+    elem_def: 104,
+    accuracy: 80,
+    speed: 64,
+    critical: 56,
+    evasion: 64
+}
 
 // Cyrus
+const Cyrus = {
+    name: "Cyrus",
+    class: "Scholar",
+    level: 1,
+    max_hp: 200,
+    max_sp: 60,
+    phys_atk: 72,
+    elem_atk: 104,
+    phys_def: 72,
+    elem_def: 96,
+    accuracy: 80,
+    speed: 72,
+    critical: 64,
+    evasion: 80
+}
 
 // Tressa
+const Tressa = {
+    name: "Tressa",
+    class: "Merchant",
+    level: 1,
+    max_hp: 275,
+    max_sp: 50,
+    phys_atk: 88,
+    elem_atk: 88,
+    phys_def: 80,
+    elem_def: 80,
+    accuracy: 80,
+    speed: 72,
+    critical: 72,
+    evasion: 72
+}
 
 // Olberic
 const Olberic = {
