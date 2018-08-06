d81c149434560b23c3e211239976c1124f165a52
Justin Dang <dangjustintime@gmail.com>
2018-08-06 15:18:51 -0400

    added second job bonuses in objects in secondary_jobs.js

---
+++ model/secondary_jobs.js
@@ -0,0 +1,68 @@
+// secondary job stat bonuses
+// stat bonuses found from:
+//      https://www.rpgsite.net/feature/7475-octopath-traveler-job-classes-skill-list-stat-bonuses-and-tips-for-every-job-subclass-and-secondary-class
+
+// warrior bonuses
+const WarriorBonuses = {
+    max_hp: 0.18,
+    phys_atk: 0.05,
+    phys_def: 0.05
+}
+
+// hunter bonuses
+const HunterBonuses = {
+    accuracy: 0.06,
+    critical: 0.06,
+    evasion: 0.05,
+    phys_atk: 0.08,
+    speed: 0.05
+}
+
+// scholar bonuses
+const ScholarBonuses = {
+    elem_atk: 0.1,
+    elem_def: 0.05,
+    max_sp: 0.13
+}
+
+// cleric bonuses
+const ClericBonuses = {
+    elem_atk: 0.05,
+    elem_def: 0.05,
+    max_sp: 0.18
+}
+
+// apothecary bonuses
+const ApothecaryBonuses = {
+    critical: 0.015,
+    elem_atk: 0.015,
+    max_hp: 0.2,
+    phys_atk: 0.015,
+    phys_def: 0.015
+}
+
+// dancer bonuses
+const DancerBonuses = {
+    elem_atk: 0.08,
+    evasion: 0.1,
+    speed: 0.1
+}
+
+// merchant bonuses
+const MerchantBonuses = {
+    elem_atk: 0.03,
+    elem_def: 0.03,
+    max_hp: 0.08,
+    max_sp: 0.08,
+    phys_atk: 0.03,
+    phys_def: 0.03
+}
+
+// theif bonuses
+const TheifBonuses = {
+    accuracy: 0.05,
+    critical: 0.05,
+    evasion: 0.08,
+    phys_atk: 0.02,
+    speed: 0.08
+}
